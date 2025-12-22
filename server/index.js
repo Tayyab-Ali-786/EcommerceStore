345f4fd44bbe1c3a5f4c4d9128145d9a5f9d44eb
Tayyab ali <f240704@cfd.nu.edu.pk>
2025-12-23 02:22:16 +0500

    updated backend deployement

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
+const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+const app = express();
+
 const allowedOrigins = [
-  "http://localhost:3001", // local dev
-  "https://my-app-one-inky.vercel.app" // production
+  "http://localhost:3001",
+  "https://my-app-one-inky.vercel.app"
 ];
 
 app.use(
@@ -21,33 +24,26 @@ app.use(
   })
 );
 
-// IMPORTANT: handle preflight
+// handle preflight
 app.options("*", cors());
-
-
-mongoose.connect("mongodb://localhost:27017/ecommerceStore")
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log("MongoDB Connection Error:", err));
-
-
-const express = require("express");
-const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
+
+mongoose.connect("mongodb://localhost:27017/ecommerceStore")
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log("MongoDB Connection Error:", err));
 
 const userModel = require("./models/user");
 const productModel = require("./models/product");
 const cartModel = require("./models/cart");
 const orderModel = require("./models/orderModel");
 
-
 const addUser = require("./controllers/UserControllers/addUser");
-const getSingleUser = require("./controllers/UserControllers/getSingleUser")
+const getSingleUser = require("./controllers/UserControllers/getSingleUser");
 const updateUser = require("./controllers/UserControllers/updateUser");
 const deleteUser = require("./controllers/UserControllers/deleteUser");
-
 
 const addProduct = require("./controllers/ProductControllers/addProduct");
 const product = require("./controllers/ProductControllers/getAllProducts");
@@ -55,28 +51,22 @@ const singleProduct = require("./controllers/ProductControllers/getSingleProduct
 const updatedProduct = require("./controllers/ProductControllers/updateProduct");
 const deletedProduct = require("./controllers/ProductControllers/deleteProduct");
 
-
 const addCart = require("./controllers/CartController/addCart");
 const getCart = require("./controllers/CartController/getCart");
 const deleteCart = require("./controllers/CartController/deleteCart");
 const searchCart = require("./controllers/CartController/SearchCart");
 
-
 const searchOrders = require("./controllers/OrderController/searchOrder");
 const searchDeletedOrders = require("./controllers/OrderController/deleteOrder");
 
-
-
 app.get("/", (req, res) => {
   res.send("Testing server");
-})
-
+});
 
 app.route("/addUser").post(addUser);
 app.route("/users/:userid").get(getSingleUser);
 app.route("/users/update/:userid").put(updateUser);
 app.route("/users/delete/:userid").delete(deleteUser);
-
 
 app.route("/addProduct").post(addProduct);
 app.route("/products").get(product);
@@ -84,17 +74,13 @@ app.route("/products/:productId").get(singleProduct);
 app.route("/products/update/:productId").put(updatedProduct);
 app.route("/products/delete/:productId").delete(deletedProduct);
 
-
 app.route("/addCart").post(addCart);
 app.route("/getCart/:userId").get(getCart);
 app.route("/deleteCart/:userId").delete(deleteCart);
 app.route("/cart/:userId/clear").delete(searchCart);
 
-
 app.route("/orders/:userId").get(searchOrders);
-
 app.route("/order/:userId").delete(searchDeletedOrders);
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
