8a08f82d3051a4d3c7dd62559694190bb7d5dfe7
Tayyab ali <f240704@cfd.nu.edu.pk>
2025-12-23 02:26:21 +0500

    wth bro if this time deployemt fails this project is being dumped

--- server/index.js
+++ server/index.js
@@ -24,63 +24,36 @@ app.use(
   })
 );
 
-// handle preflight
-app.options("*", cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
-mongoose.connect("mongodb://localhost:27017/ecommerceStore")
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log("MongoDB Connection Error:", err));
 
-const userModel = require("./models/user");
-const productModel = require("./models/product");
-const cartModel = require("./models/cart");
-const orderModel = require("./models/orderModel");
+// routes
+app.get("/", (req, res) => res.send("Testing server"));
 
-const addUser = require("./controllers/UserControllers/addUser");
-const getSingleUser = require("./controllers/UserControllers/getSingleUser");
-const updateUser = require("./controllers/UserControllers/updateUser");
-const deleteUser = require("./controllers/UserControllers/deleteUser");
+app.route("/addUser").post(require("./controllers/UserControllers/addUser"));
+app.route("/users/:userid").get(require("./controllers/UserControllers/getSingleUser"));
+app.route("/users/update/:userid").put(require("./controllers/UserControllers/updateUser"));
+app.route("/users/delete/:userid").delete(require("./controllers/UserControllers/deleteUser"));
 
-const addProduct = require("./controllers/ProductControllers/addProduct");
-const product = require("./controllers/ProductControllers/getAllProducts");
-const singleProduct = require("./controllers/ProductControllers/getSingleProduct");
-const updatedProduct = require("./controllers/ProductControllers/updateProduct");
-const deletedProduct = require("./controllers/ProductControllers/deleteProduct");
+app.route("/addProduct").post(require("./controllers/ProductControllers/addProduct"));
+app.route("/products").get(require("./controllers/ProductControllers/getAllProducts"));
+app.route("/products/:productId").get(require("./controllers/ProductControllers/getSingleProduct"));
+app.route("/products/update/:productId").put(require("./controllers/ProductControllers/updateProduct"));
+app.route("/products/delete/:productId").delete(require("./controllers/ProductControllers/deleteProduct"));
 
-const addCart = require("./controllers/CartController/addCart");
-const getCart = require("./controllers/CartController/getCart");
-const deleteCart = require("./controllers/CartController/deleteCart");
-const searchCart = require("./controllers/CartController/SearchCart");
+app.route("/addCart").post(require("./controllers/CartController/addCart"));
+app.route("/getCart/:userId").get(require("./controllers/CartController/getCart"));
+app.route("/deleteCart/:userId").delete(require("./controllers/CartController/deleteCart"));
+app.route("/cart/:userId/clear").delete(require("./controllers/CartController/SearchCart"));
 
-const searchOrders = require("./controllers/OrderController/searchOrder");
-const searchDeletedOrders = require("./controllers/OrderController/deleteOrder");
-
-app.get("/", (req, res) => {
-  res.send("Testing server");
-});
-
-app.route("/addUser").post(addUser);
-app.route("/users/:userid").get(getSingleUser);
-app.route("/users/update/:userid").put(updateUser);
-app.route("/users/delete/:userid").delete(deleteUser);
-
-app.route("/addProduct").post(addProduct);
-app.route("/products").get(product);
-app.route("/products/:productId").get(singleProduct);
-app.route("/products/update/:productId").put(updatedProduct);
-app.route("/products/delete/:productId").delete(deletedProduct);
-
-app.route("/addCart").post(addCart);
-app.route("/getCart/:userId").get(getCart);
-app.route("/deleteCart/:userId").delete(deleteCart);
-app.route("/cart/:userId/clear").delete(searchCart);
-
-app.route("/orders/:userId").get(searchOrders);
-app.route("/order/:userId").delete(searchDeletedOrders);
+app.route("/orders/:userId").get(require("./controllers/OrderController/searchOrder"));
+app.route("/order/:userId").delete(require("./controllers/OrderController/deleteOrder"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
